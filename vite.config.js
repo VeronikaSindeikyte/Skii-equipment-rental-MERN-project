8c77a67da2a4225bcbeb2758ba150566d704e07d
VeronikaSindeikyte <veronika.sindeikyte@gmail.com>
2025-02-18 18:31:54 +0200

    fixed vite config file

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,37 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:4001',
+//         changeOrigin: true,
+//         secure: false,
+//       }
+//     }
+//   }
+// });
+
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4001',
+        target: process.env.NODE_ENV === 'production'
+          ? 'https://skii-equipment-rental-mern-project.onrender.com'
+          : 'http://localhost:4001',
         changeOrigin: true,
         secure: false,
-      }
-    }
-  }
+      },
+    },
+  },
+  define: {
+    'process.env.API_BASE_URL': JSON.stringify(
+      process.env.NODE_ENV === 'production'
+        ? 'https://skii-equipment-rental-mern-project.onrender.com'
+        : 'http://localhost:4001'
+    ),
+  },
 });
