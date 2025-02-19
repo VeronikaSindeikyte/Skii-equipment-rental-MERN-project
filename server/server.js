c62a8c940db6bacbc9fda9ba02237f305c81b9da
VeronikaSindeikyte <veronika.sindeikyte@gmail.com>
2025-02-19 11:02:01 +0200

    cors access fixed

--- server/server.js
+++ server/server.js
@@ -22,6 +22,13 @@ app.use(cors({
     credentials: true
   }));
 
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization');
+    next();
+});
+
 const PORT = process.env.PORT || 4001;
 
 
